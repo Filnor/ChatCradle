4c66594d7d2022a829c54d4ef233c5c1934fde27
Filnor <philipp.bergamin@gmail.com>
2018-10-18 13:35:19 +0200

    Multi-room capability (closes #1)

--- chatcradle.py
+++ chatcradle.py
@@ -1,19 +1,31 @@
 import time
-from chatexchange.chatexchange.client import Client
+from chatoverflow.chatexchange.client import Client
 
 def main():
+    room_queue = [ #Add a comma-separated list of all rooms, in which the bot should join, here
+        167908, #SOBotics Workshop
+        163468 #Filnor's HQ
+    ]
+    rooms = []
 
     print("Logging in...")
     #Login and connection to chat
     client = Client("stackoverflow.com")
     client.login("<email>", "<password>")
-    room = client.get_room(<room number>)
-    room.join()
-    print("Joined room.")
+
+    #Get object instances for the room(s)
+    for room_to_join in room_queue:
+        rooms.append(client.get_room(room_to_join))
+
+    for room in rooms:
+        room.join()
+
+    print("Joined room(s).")
 
     while True:
-        room.send_message("[ [ChatCradle](https://stackoverflow.com/users/4733879/filnor) ] Keeping the room fresh...")
-        print("Sent message")
+        for room in rooms:
+            room.send_message("[ [ChatCradle](https://git.io/fxa52) ] Keeping the room fresh...")
+            print(f"Sent keep-alive message to room {room.name} (id {room.id})")
         time.sleep(86400)
 
 if __name__ == '__main__':
